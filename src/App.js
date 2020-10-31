6d8766c005f4cf8934c6e83b0cb0d57c90253e38
Hamza Alvi <42583422+hamzaalvi1@users.noreply.github.com>
2020-11-01 02:43:57 +0500

    add ui of consultant-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import ConsultantPage from "./Components/Consultant/ConsultantPage"
+import Skills from "./Components/Skills/Skills"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid>
+      <Row>
+      <Col xs = {12} sm = {12} md = {6} lg = {5}>
+      <ConsultantPage/>
+      </Col>
+      <Col xs = {12} sm = {12} md = {6} lg = {7}>
+      <Skills/>
+      </Col>
+      </Row> 
+      </Container>
     </div>
   );
 }
